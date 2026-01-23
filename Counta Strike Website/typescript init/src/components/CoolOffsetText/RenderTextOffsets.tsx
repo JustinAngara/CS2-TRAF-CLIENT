9e49893f397bc5b7e45709430d216aff1e9057bc
Justin Angara <justin.angara@gmail.com>
2026-01-23 04:26:24 -0600

    more bootstrap, i need more original thoughts

--- Counta Strike Website/typescript init/src/components/CoolOffsetText/RenderTextOffsets.tsx
+++ Counta Strike Website/typescript init/src/components/CoolOffsetText/RenderTextOffsets.tsx
@@ -1,12 +1,38 @@
 import type { ReactNode, CSSProperties } from "react";
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import React, { useState } from "react";
 
+/*
+* client.dll, server.dll, offsets.dll, buttons.dll
+* */
 const RenderTextOffsets = () => {
+	const defaultTab = "client";
+	const [key, setKey] = useState(defaultTab);
+
 	return (
 		<div style={styles.root}>
-		{/*	client.dll */}
-		{/*	server.dll */}
-		{/*	offsets.dll */}
-		{/*	buttons.dll */}
+			<Tabs
+				defaultActiveKey={ defaultTab }
+				id="tabs"
+				activeKey={ key }
+				onSelect={(k) => k && setKey(k)}
+				className="mb-3"
+				transition={false}  fill
+			>
+				<Tab eventKey="client" title="client.dll">
+					Tab content for Client
+				</Tab>
+				<Tab eventKey="server" title="server.dll">
+					Tab content for Server
+				</Tab>
+				<Tab eventKey="offsets" title="offsets.dll">
+					Tab content for Offsets
+				</Tab>
+				<Tab eventKey="buttons" title="buttons.dll">
+					Tab content for Buttons
+				</Tab>
+			</Tabs>
 
 		</div>
 	);
