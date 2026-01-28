774c035778c63eb242e3ce661fe09f364b47f790
Justin Angara <justin.angara@gmail.com>
2026-01-28 05:46:11 -0600

    kat

--- cheeto codebase/Internal Base/src/sdk/memory/Offsets.h
+++ cheeto codebase/Internal Base/src/sdk/memory/Offsets.h
@@ -1,27 +1,6 @@
 #pragma once
 #include <cstddef>
 
-/*
-* NOTE: Hopefully I RE correctly ;-;
-	offsets are going to be rolled back to the 1/19/26 version
-	i will figure out the changes soon
-	use old cs2 version dating 1/19/26
-	constexpr uintptr_t		 dwEntityList			 = 0x21C3AF8;
-	constexpr uintptr_t		 dwLocalPlayerPawn		 = 0x2061850;
-	constexpr uintptr_t		 dwLocalPlayerController = 0x22EBA28;
-	constexpr uintptr_t		 dwViewMatrix			 = 0x2307850;
-	constexpr uintptr_t		 dwViewAngles			 = 0x2311A68;
-	constexpr uintptr_t		 dwGlobalVars			 = 0x20562A8;
-	constexpr std::ptrdiff_t dwSensitivity			 = 0x2303128;
-
-	constexpr uintptr_t		 dwEntityList			 = 0x1D13CE8;
-	constexpr uintptr_t		 dwLocalPlayerPawn		 = 0x1BEEF28;
-	constexpr uintptr_t		 dwLocalPlayerController = 0x1E1DC18;
-	constexpr uintptr_t		 dwViewMatrix			 = 0x1E323D0;
-	constexpr uintptr_t		 dwViewAngles			 = 0x1E3C800;
-	constexpr uintptr_t		 dwGlobalVars			 = 0x1BE41C0;
-	constexpr std::ptrdiff_t dwSensitivity			 = 0x1E2ED08;
-*/
 
 namespace Offsets
 {
