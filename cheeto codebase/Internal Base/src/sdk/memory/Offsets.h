91f62ccadf2203c5712121796b769bdf929568ae
Justin Angara <justin.angara@gmail.com>
2026-02-04 21:28:56 -0600

    update offsets

--- cheeto codebase/Internal Base/src/sdk/memory/Offsets.h
+++ cheeto codebase/Internal Base/src/sdk/memory/Offsets.h
@@ -5,13 +5,13 @@
 namespace Offsets
 {
 	// -> offsets.h on a2x dumper
-	constexpr uintptr_t		 dwEntityList			 = 0x24A7B28; 
-	constexpr uintptr_t		 dwLocalPlayerPawn		 = 0x2062850; 
-	constexpr uintptr_t		 dwLocalPlayerController = 0x22ECA28; 
-	constexpr uintptr_t		 dwViewMatrix			 = 0x2308860; 
-	constexpr uintptr_t		 dwViewAngles			 = 0x2312BD8; 
-	constexpr uintptr_t		 dwGlobalVars			 = 0x20572A8;  
-	constexpr std::ptrdiff_t dwSensitivity			 = 0x2304128; 
+	constexpr uintptr_t		 dwEntityList			 = 0x24ABF98; //
+	constexpr uintptr_t		 dwLocalPlayerPawn		 = 0x2066AE0; //
+	constexpr uintptr_t		 dwLocalPlayerController = 0x22F0FB8; // 
+	constexpr uintptr_t		 dwViewMatrix			 = 0x230CC90; //
+	constexpr uintptr_t		 dwViewAngles			 = 0x2317708; //
+	constexpr uintptr_t		 dwGlobalVars			 = 0x205B630; //   
+	constexpr std::ptrdiff_t dwSensitivity			 = 0x2308568; 
 
 	// -> client.dll.hpp on a2x dumper -> C_BaseEntity
 	constexpr uintptr_t m_iHealth		 = 0x354;
@@ -41,9 +41,9 @@ namespace Offsets
 	constexpr std::ptrdiff_t m_iObserverMode	  = 0x48;
 	constexpr std::ptrdiff_t m_entitySpottedState = 0x1F98;
 	constexpr std::ptrdiff_t m_aimPunchAngleVel	  = 0x16D8;
-	constexpr std::ptrdiff_t m_aimPunchCache = 0x16F0;
+	constexpr std::ptrdiff_t m_aimPunchCache	  = 0x16F0;
 
 	// -> Buttons
-	constexpr std::ptrdiff_t jump			 = 0x205BAF0;
+	constexpr std::ptrdiff_t jump = 0x205BAF0;
 
 }
