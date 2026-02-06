859a7cf7d881b5a21728bef3d44349914bff86e7
Justin Angara <justin.angara@gmail.com>
2026-02-05 19:31:25 -0600

    please tell me i didn't screw up anything

--- cheeto codebase/Internal Base/src/sdk/memory/OffsetGrabber/OffsetGrabber.h
+++ cheeto codebase/Internal Base/src/sdk/memory/OffsetGrabber/OffsetGrabber.h
@@ -6,6 +6,7 @@ this will be the setup to grab everything at init
 i am very sick of having to manually find offsets and then manually have to type it out.
 check out: https://justinangara.github.io/CS2-TRAF-CLIENT/
 ferda
+okay this is a test sorry for spamming commits
 */
 namespace OffsetGrabber
 {
