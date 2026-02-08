f27dbb5fbcc0f49c148ffbd5c08ccb6ab4bdd67c
Justin Angara <justin.angara@gmail.com>
2026-02-08 11:13:55 -0600

    setup dir for inject

--- cheeto codebase/Loader/Loader.h
+++ cheeto codebase/Loader/Loader.h
@@ -7,14 +7,14 @@
 
 namespace Loader
 {
-	LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
-	int WINAPI		 Run(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR cmdLine, int nCmdShow);
+    LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+    int WINAPI Run(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR cmdLine, int nCmdShow);
 
-	void		 OnJsMessage(const std::wstring& msg);
-	std::wstring PathToFileUri(const std::wstring& path);
-	std::wstring ExeDir();
+    void OnJsMessage(const std::wstring& msg);
+    std::wstring PathToFileUri(const std::wstring& path);
+    std::wstring ExeDir();
 
-	inline HWND											   g_hwnd = nullptr;
-	inline Microsoft::WRL::ComPtr<ICoreWebView2Controller> g_controller{};
-	inline Microsoft::WRL::ComPtr<ICoreWebView2>		   g_webview{};
+    inline HWND g_hwnd = nullptr;
+    inline Microsoft::WRL::ComPtr<ICoreWebView2Controller> g_controller{};
+    inline Microsoft::WRL::ComPtr<ICoreWebView2> g_webview{};
 }
