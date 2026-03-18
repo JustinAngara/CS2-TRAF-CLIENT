a0076f19e3124591a00ed15ef1e03e7088f68ce7
Justin Angara <justin.angara@gmail.com>
2026-03-17 23:29:30 -0500

    update offsets

--- cheeto codebase/Internal Base/src/sdk/memory/Offsets.h
+++ cheeto codebase/Internal Base/src/sdk/memory/Offsets.h
@@ -8,19 +8,19 @@
 namespace Offsets
 {
 	// -> offsets.h on a2x dumper
-	constexpr uintptr_t		 dwEntityList			 = 0x24AA0D8; 
-	constexpr uintptr_t		 dwLocalPlayerPawn		 = 0x2064AE0; 
-	constexpr uintptr_t		 dwLocalPlayerController = 0x22EF0B8;  
-	constexpr uintptr_t		 dwViewMatrix			 = 0x230ADE0; 
-	constexpr uintptr_t		 dwViewAngles			 = 0x2314F98; 
-	constexpr uintptr_t		 dwGlobalVars			 = 0x20595D0;   
-	constexpr std::ptrdiff_t dwSensitivity			 = 0x23066B8; 
+	constexpr uintptr_t		 dwEntityList			 = 0x24AE268; // 
+	constexpr uintptr_t		 dwLocalPlayerPawn		 = 0x2068B60; //
+	constexpr uintptr_t		 dwLocalPlayerController = 0x22F3178; // 
+	constexpr uintptr_t		 dwViewMatrix			 = 0x230EF20; // 
+	constexpr uintptr_t		 dwViewAngles			 = 0x2319648; // 
+	constexpr uintptr_t		 dwGlobalVars			 = 0x205D5C0; //   
+	constexpr std::ptrdiff_t dwSensitivity			 = 0x230A7F8; // 
 
 	// -> client.dll.hpp on a2x dumper -> C_BaseEntity
-	constexpr uintptr_t m_iHealth		 = 0x354;
-	constexpr uintptr_t m_iTeamNum		 = 0x3F3;
-	constexpr uintptr_t m_vOldOrigin	 = 0x1588;
-	constexpr uintptr_t m_pGameSceneNode = 0x338;
+	constexpr uintptr_t m_iHealth		 = 0x354; //
+	constexpr uintptr_t m_iTeamNum		 = 0x3F3; //
+	constexpr uintptr_t m_vOldOrigin	 = 0x1588; // 
+	constexpr uintptr_t m_pGameSceneNode = 0x338; 
 
 	// -> CbaseModelEntity
 	constexpr uintptr_t m_vecViewOffset = 0xD58;
@@ -42,7 +42,7 @@ namespace Offsets
 	constexpr std::ptrdiff_t m_pObserverServices  = 0x13F0;
 	constexpr std::ptrdiff_t m_hObserverTarget	  = 0x4C;
 	constexpr std::ptrdiff_t m_iObserverMode	  = 0x48;
-	constexpr std::ptrdiff_t m_entitySpottedState = 0x1F98;
+	constexpr std::ptrdiff_t m_entitySpottedState = 0x1180; //
 	constexpr std::ptrdiff_t m_aimPunchAngleVel	  = 0x16D8;
 	constexpr std::ptrdiff_t m_aimPunchCache	  = 0x16F0;
 
