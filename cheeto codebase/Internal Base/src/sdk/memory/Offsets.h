f7f2d991a8b9716f57a18940f3724b245e535ccf
Justin Angara <justin.angara@gmail.com>
2026-03-17 23:30:48 -0500

    cleanup

--- cheeto codebase/Internal Base/src/sdk/memory/Offsets.h
+++ cheeto codebase/Internal Base/src/sdk/memory/Offsets.h
@@ -3,23 +3,24 @@
 
 /*
 	going to switch up how we do the offsets
+	utilzie the loader to check for updates		
 */
 
 namespace Offsets
 {
 	// -> offsets.h on a2x dumper
-	constexpr uintptr_t		 dwEntityList			 = 0x24AE268; // 
-	constexpr uintptr_t		 dwLocalPlayerPawn		 = 0x2068B60; //
-	constexpr uintptr_t		 dwLocalPlayerController = 0x22F3178; // 
-	constexpr uintptr_t		 dwViewMatrix			 = 0x230EF20; // 
-	constexpr uintptr_t		 dwViewAngles			 = 0x2319648; // 
-	constexpr uintptr_t		 dwGlobalVars			 = 0x205D5C0; //   
-	constexpr std::ptrdiff_t dwSensitivity			 = 0x230A7F8; // 
+	constexpr uintptr_t		 dwEntityList			 = 0x24AE268;  
+	constexpr uintptr_t		 dwLocalPlayerPawn		 = 0x2068B60; 
+	constexpr uintptr_t		 dwLocalPlayerController = 0x22F3178;  
+	constexpr uintptr_t		 dwViewMatrix			 = 0x230EF20;  
+	constexpr uintptr_t		 dwViewAngles			 = 0x2319648;  
+	constexpr uintptr_t		 dwGlobalVars			 = 0x205D5C0;    
+	constexpr std::ptrdiff_t dwSensitivity			 = 0x230A7F8;  
 
-	// -> client.dll.hpp on a2x dumper -> C_BaseEntity
-	constexpr uintptr_t m_iHealth		 = 0x354; //
-	constexpr uintptr_t m_iTeamNum		 = 0x3F3; //
-	constexpr uintptr_t m_vOldOrigin	 = 0x1588; // 
+	////////////// -> client.dll.hpp on a2x dumper and on
+	constexpr uintptr_t m_iHealth		 = 0x354; 
+	constexpr uintptr_t m_iTeamNum		 = 0x3F3; 
+	constexpr uintptr_t m_vOldOrigin	 = 0x1588; 
 	constexpr uintptr_t m_pGameSceneNode = 0x338; 
 
 	// -> CbaseModelEntity
@@ -30,23 +31,19 @@ namespace Offsets
 	constexpr uintptr_t m_aimPunchAngle = 0x16CC;
 	constexpr uintptr_t m_fFlags		= 0x400;
 
-	// -> C_CSPlayerController
-	constexpr uintptr_t m_szTeamname   = 0x63C; // idk the offset
+	constexpr uintptr_t m_szTeamname   = 0x63C; 
 	constexpr uintptr_t m_hPlayerPawn  = 0x90C;
 	constexpr uintptr_t m_bPawnIsAlive = 0x914;
 
-	// -> CBodyComponentSkeletonInstance
 	constexpr uintptr_t m_modelState = 0x160;
 
-	// -> CObserverServices
 	constexpr std::ptrdiff_t m_pObserverServices  = 0x13F0;
 	constexpr std::ptrdiff_t m_hObserverTarget	  = 0x4C;
 	constexpr std::ptrdiff_t m_iObserverMode	  = 0x48;
-	constexpr std::ptrdiff_t m_entitySpottedState = 0x1180; //
+	constexpr std::ptrdiff_t m_entitySpottedState = 0x1180; 
 	constexpr std::ptrdiff_t m_aimPunchAngleVel	  = 0x16D8;
 	constexpr std::ptrdiff_t m_aimPunchCache	  = 0x16F0;
 
-	// -> Buttons
 	constexpr std::ptrdiff_t jump = 0x205BAF0;
 
 }
